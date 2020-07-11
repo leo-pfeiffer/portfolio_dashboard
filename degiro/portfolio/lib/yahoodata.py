c3e2efe422fcb86eee8bb75310397f0e42c197ab
leopold-pfeiffer <leopold.pfeiffer@gmx.de>
2020-07-11 10:46:36 +0200

    Add ffill_yahoo_data

--- degiro/portfolio/lib/yahoodata.py
+++ degiro/portfolio/lib/yahoodata.py
@@ -1,10 +1,12 @@
 import yfinance as yf
+import pandas as pd
 import datetime
-
+from dateutil.relativedelta import relativedelta
 
 def get_yahoo_data(tickers: list, start: str, end: str):
     tickers = [t.lower() for t in tickers]
     prices = yf.download(tickers=tickers, start=start, end=end).loc[:, 'Adj Close']
+    prices.index = [x.date() for x in prices.index.to_list()]
     # todo: the following line is very slow.. maybe use other package/source
     currencies = {ticker: yf.Ticker(ticker).info['currency'] for ticker in tickers}
     return {'prices': prices, 'currencies': currencies}
@@ -15,6 +17,16 @@ def last_data_at_date(tickers: list, date: datetime.datetime.date):
     pass
 
 
+def ffill_yahoo_data(df: pd.DataFrame) -> pd.DataFrame:
+    start = df.index[0].date()
+    end = df.index[-1].date()
 
-if __name__=='__main__':
+    daterange = [start + relativedelta(days=i) for i in range((end - start).days + 1)]
+    new = pd.DataFrame(index=daterange)
+
+    return pd.merge(new, df, left_index=True, right_index=True, how='left').ffill()
+
+
+if __name__ == '__main__':
+    get_yahoo_data(['msft', 'amzn', 'tsla'], start="2020-01-10", end="2020-01-25")
     int(0)
