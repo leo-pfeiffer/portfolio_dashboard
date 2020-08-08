37a7fe38a3b0cd1b81fa4235db89c0b1e8ca2244
leopold-pfeiffer <leopold.pfeiffer@gmx.de>
2020-08-08 18:09:44 +0200

    mail debug

--- degiro/portfolio/lib/helpers.py
+++ degiro/portfolio/lib/helpers.py
@@ -1,11 +1,10 @@
 import datetime
 import json
 import smtplib
-import email
 
 from email import encoders
-from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
 from email.mime.text import MIMEText
 
 import ssl
@@ -17,15 +16,12 @@ def daterange(start_date, end_date):
         yield start_date + datetime.timedelta(n)
 
 
-def send_email(recipients: str, subject: str, body: str, filename: str):
+def send_email(receiver_email: str, subject: str, body: str, filename: str):
     conf_path = paths.SETTINGS + '/mail.json'
     conf = json.load(open(conf_path))
 
-    port = 587  # For starttls
-    smtp_server = "smtp.gmail.com"
-    sender_email = conf['email']  # Enter your address
-    # receiver_email = recipients
-    receiver_email = "leopold.pfeiffer@gmx.de"
+    smtp_server = conf['smtp']
+    sender_email = conf['email']
     password = conf['password']
 
     # Create a multipart message and set headers
@@ -38,12 +34,9 @@ def send_email(recipients: str, subject: str, body: str, filename: str):
     message.attach(MIMEText(body, "plain"))
 
     if filename is not None:
-        # Open PDF file in binary mode
+
         with open(filename, "rb") as attachment:
-            # Add file as application/octet-stream
-            # Email client can usually download this automatically as attachment
-            part = MIMEBase("application", "octet-stream")
-            part.set_payload(attachment.read())
+            part = MIMEApplication(attachment.read(), _subtype="pdf")
 
         # Encode file in ASCII characters to send by email
         encoders.encode_base64(part)
@@ -57,8 +50,6 @@ def send_email(recipients: str, subject: str, body: str, filename: str):
         message.attach(part)
 
     text = message.as_string()
-
-    # Log in to server using secure context and send email
 
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(smtp_server, 465, context=context) as server:
