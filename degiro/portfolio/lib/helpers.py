2c5c2a8561367602c09a69ac067d29de75b87f70
leopold-pfeiffer <leopold.pfeiffer@gmx.de>
2020-08-09 08:54:55 +0200

    implement measures

--- degiro/portfolio/lib/helpers.py
+++ degiro/portfolio/lib/helpers.py
@@ -1,7 +1,5 @@
 import datetime
 import json
-import numpy as np
-from scipy.stats import norm, t
 import smtplib
 
 from email import encoders
@@ -58,48 +56,3 @@ def send_email(receiver_email: str, subject: str, body: str, filename: str):
         server.sendmail(sender_email, receiver_email, text)
 
 
-# Measures ---
-
-def returns(series):
-    return {returns.__name__: (series[-1] / series[0]) - 1}
-
-
-def annualized_returns(series):
-    return {annualized_returns.__name__: (series[-1] / series[0]) ** (252 / len(series)) - 1}
-
-
-def std(series):
-    return {std.__name__: np.std(series.pct_change(), ddof=1) * np.sqrt(252)}
-
-
-def sharpe(series):
-    mean = annualized_returns(series)["annualized_returns"]
-    sd = std(series)["std"]
-    rf = pd.read_csv("yahoo_data.csv", index_col="Date", parse_dates=True)["^IRX"][series.index[0]:series.index[-1]]
-    ex_return = mean - np.mean(rf)
-    return {sharpe.__name__: ex_return / sd}
-
-
-def var(series):
-    series = series.pct_change().dropna()
-    mean = np.mean(series)
-    sd = np.std(series)
-
-    return {var.__name__: norm.ppf(0.01, loc=mean, scale=sd)}
-
-
-def cvar(series):
-    series = series.pct_change()
-    mean = np.mean(series)
-    sd = np.std(series)
-
-    return {cvar.__name__: mean - 0.01 ** (-1) * sd * norm.pdf(norm.ppf(0.01))}
-
-
-def max_drawdown(series):
-    window = len(series)
-    roll_max = series.rolling(window, min_periods=1).max()
-    daily_drawdown = series / roll_max - 1
-    max_daily_drawdown = daily_drawdown.rolling(window, min_periods=1).min()
-
-    return {max_drawdown.__name__: min(max_daily_drawdown)}
